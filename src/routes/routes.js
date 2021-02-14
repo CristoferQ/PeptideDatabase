633fdd86cde202c5d6fec233e7b7e06f5b09816b
Cristofer Quiroz <ceql@live.cl>
2021-02-14 03:27:28 -0300

    Upload Files

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,8 @@
 const {Router} = require('express');
 const router = Router();
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, './src/public/jobs/service6')
-    },
-    filename: function(req, file, cb){
-        cb(null, file.fieldname)
-    }
-})
-const upload = multer({storage})
+var multer = require("multer");
+var url = require('url');
+//var upload = multer({dest: './src/public/jobs/service6/tmp'});
 
 const {getClassification, renderGlossary, getTraining, renderTraining, getEncoding, renderEncoding, getFrequency, renderFrequency, getAlignment, getCharacterization, renderAlignment, renderClassification, renderCharacterization, renderDatabaseInformation,renderSequence, getSearch, getDatabasePerActivity, renderDetails, renderAbout, renderSearch, renderTools, renderIndex, renderDatabase, getDatabase} = require('../controllers/controller');
 
@@ -36,7 +28,54 @@ router.post('/frequency/results', getFrequency);
 router.get('/encoding', renderEncoding);
 router.post('/encoding/results', getEncoding);
 router.get('/training', renderTraining);
-router.post('/training/results', upload.single('file'), getTraining);
+
+var storage1 = multer.diskStorage({ 
+    
+    destination: function (req, file, cb) { 
+        cb(null, "./src/public/jobs/service6/tmp") 
+    }, 
+    filename: function (req, file, cb) { 
+        cb(null, file.fieldname+".csv") 
+        console.log(req.url)
+        var url_parts = url.parse(req.url, true);
+        var query = url_parts.query;
+        console.log(query.time)
+    } 
+}) 
+
+var upload1 = multer({  
+    storage: storage1,  
+}).any();//array(['dataset', 'response']); 
+
+//var upload2 = multer({  
+//    storage: storage2,  
+//}).single("response"); 
+
+
+
+router.post("/training/upload",function (req, res, next) {     
+    upload1(req,res,function(err) { 
+        if(err) { 
+            res.send(err) 
+        } 
+        else { 
+            console.log("Success, response uploaded!") 
+            res.send({"response":"ok"})
+        } 
+    }) 
+    //upload2(req,res,function(err) { 
+    //    if(err) { 
+    //        res.send(err) 
+    //    } 
+    //    else { 
+    //        console.log("Success, response uploaded!") 
+    //    } 
+    //}) 
+}) 
+
+
+router.post('/training/results', getTraining);
+
 router.get('/glossary', renderGlossary);
 
 module.exports = router;
